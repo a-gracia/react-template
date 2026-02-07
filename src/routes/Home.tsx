549db4f8cde492286934253a389dacc1c3b027db
Andres <18741308+a-gracia@users.noreply.github.com>
2026-02-06 22:01:45 -0500

    fix: improves defaults

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,17 +1,7 @@
-import { Link } from "react-router";
-
 const Home = () => {
   return (
     <div>
-      <h1>Hello from the main page of the app!</h1>
-      <p>Here are some examples of links to other pages</p>
-      <nav>
-        <ul>
-          <li>
-            <Link to="profile">Profile page</Link>
-          </li>
-        </ul>
-      </nav>
+      <h1>Home</h1>
     </div>
   );
 };
